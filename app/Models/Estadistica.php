f53fdebefb45faeba05c822daf432b7c6cab639b
DRAGO <73708124+Dragoadri@users.noreply.github.com>
2024-04-12 13:51:33 +0200

    est y partis

--- app/Models/Estadistica.php
+++ app/Models/Estadistica.php
@@ -7,6 +7,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estadistica extends Model
 {
+    use HasFactory;
+
+    protected $table = 'estadisticas';
+    protected $primaryKey = 'est_id';
+    protected $fillable = [
+        'md_id',
+        'jornada_id',
+        'puntos',
+        'total_mins_played',
+        'total_penalty_save',
+        'total_saves',
+        'total_effective_clearance',
+        'total_goals_conceded',
+        'total_yellow_card',
+        'total_ball_recovery',
+        'total_poss_lost_all',
+        'score_marca_points',
+        'total_goals',
+        'total_goal_assist',
+        'total_offtarget_att_assist',
+        'total_pen_area_entries',
+        'total_penalty_won',
+        'total_total_scoring_att',
+        'total_won_contest',
+        'total_penalty_failed',
+        'total_penalty_conceded',
+        'total_second_yellow_card',
+        'total_own_goals',
+        'total_red_card',
+    ];
+
+    public function jugador()
+    {
+        return $this->belongsTo(Jugador::class, 'md_id', 'md_id');
+    }
+
+    public function jornada()
+    {
+        return $this->belongsTo(Jornada::class, 'jornada_id', 'jornada_id');
+    }
+
+
 
 
 }
--- app/Models/Partido.php
+++ app/Models/Partido.php
@@ -5,7 +5,58 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//La tabla es la siguiente:
+
+/**
+ * CREATE TABLE {bbdd_database_name}.partidos (
+ * jornada_id INT AUTO_INCREMENT NOT NULL,
+ * temporada TEXT NOT NULL,
+ * fecha YEAR,
+ * jornada INT,
+ * equipo_local VARCHAR(255),
+ * equipo_visitante VARCHAR(255),
+ * resultado TEXT,
+ * resultado_local INT,
+ * resultado_visitante INT,
+ * id_equipo_local INT UNSIGNED NOT NULL,
+ * id_equipo_visitante INT UNSIGNED NOT NULL,
+ * PRIMARY KEY(jornada_id),
+ * FOREIGN KEY (id_equipo_local) REFERENCES {bbdd_database_name}.equipor(team_id),
+ * FOREIGN KEY (id_equipo_visitante) REFERENCES {bbdd_database_name}.equipor(team_id)
+ * );
+ */
 class Partido extends Model
 {
+
     use HasFactory;
+
+    protected $table = 'partidos';
+    protected $primaryKey = 'jornada_id';
+    protected $fillable = [
+        'temporada',
+        'fecha',
+        'jornada',
+        'equipo_local',
+        'equipo_visitante',
+        'resultado',
+        'resultado_local',
+        'resultado_visitante',
+        'id_equipo_local',
+        'id_equipo_visitante',
+    ];
+
+    public function equipoLocal()
+    {
+        return $this->belongsTo(Equipo::class, 'id_equipo_local', 'team_id');
+    }
+
+    public function equipoVisitante()
+    {
+        return $this->belongsTo(Equipo::class, 'id_equipo_visitante', 'team_id');
+
+    }
+
+
+
+
 }
